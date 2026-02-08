4aa833a9a232be75f85fd9276686b0e680ba8c3a
ibras0696 <ibras0696@gmail.com>
2026-02-08 23:19:49 +0300

    Первое подключение бота

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-print('Test')
-print()
+import asyncio
+import logging
 
-print('я добавил еще ')
-print(2)
+from aiogram import Dispatcher, Bot
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Выход')
