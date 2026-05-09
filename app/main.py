662134381d64633caeda138d37bfa4a55f4e2bbe
 ibragim <ibras0696@gmail.com>
2026-05-09 23:19:35 +0300

    saf

--- app/main.py
+++ app/main.py
@@ -1,44 +1,75 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
 users = {
-    1: 'ibragim',
-    2: 'Islam',
-    3: 'Timur'
+    1: {
+        'username': 'test',
+        'email': 'test@test.yes',
+        'age': 20
+    }
 }
 
-
-@app.get("/api/v1/")
-async def root() -> dict[str, str]:
-    return {"message": "FastApi work"}
-
-
-@app.get("/users/{user_id}")
-async def get_user(user_id: int) -> dict:
-    r = users.get(user_id)
-    if not r:
-        return {"message": "user not found", "status": 404}
-    
-    return {"user": user_id, "name": r}
-    # return {"user_id": user_id}
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    age: int
 
 
-@app.get("/user")
-async def user() -> dict[str, int]:
-    return {
-        "user_1": 1,
-        "user_2": 2,
-        "user_3": 3
+@app.delete('/users')
+async def create_user(user_data: UserCreate
+                      ) -> dict[str, str | dict]:
+    tl = len(users) + 1
+    users[tl] = {
+        'username': user_data.username,
+        'email': user_data.email,
+        'age': user_data.age
     }
+    return {"status": 'ok', 'data': users}
+
+@app.get('/users')
+async def get_users() -> dict:
+    return users
 
 
-@app.get("/health")
-async def health_check() -> dict[str, str]:
-    return {"status": "ok"}
 
 
-@app.get("/version")
-async def get_version() -> dict[str, str]:
-    return {"version": "0.1.0"}
+
+
+
+
+
+
+
+
+
+
+# users = {
+#     1: {'name': 'ibragim', 'age': 21},
+#     2: {'name': 'Islam', 'age': 19},
+#     3: {'name': 'Timur', 'age': 20}
+# }
+
+
+# @app.get("/users/{user_id}")
+# async def get_user(user_id: int) -> dict:
+#     r = users.get(user_id)
+#     if not r:
+#         raise HTTPException(status_code=404, detail="user not found")
+    
+#     return {"user": user_id, "name": r}
+#     # return {"user_id": user_id}
+
+# # http://127.0.0.1:8000/user/age?min_age=10&max_age=20
+# @app.get("/user/age")
+# async def list_users(
+#     min_age: int = 10,
+#     max_age: int = 20
+#     ) -> dict:
+#     d = {
+#         key: value for key, value in users.items() 
+#         if min_age <= value['age'] <= max_age
+#     }
+#     return d
+
