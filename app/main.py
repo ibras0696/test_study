a2f86a969c5175a492ad305de4cbe471ff83af4f
 ibragim <ibras0696@gmail.com>
2026-05-28 21:38:07 +0300

    as

--- app/main.py
+++ app/main.py
@@ -1,50 +1,37 @@
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-
-from middlewares import api_header
-from routers import router
-
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:8000"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.add_middleware(api_header.AppHeaderMiddleware)
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={
-            "title": "Главная",
-            "username": "тест",
-            "dct": {1: "test"},
-            "lst": [1, 2, 3, 4],
-            "tpl": (1, 2, 3)
-        },
-    )
 
 
-@app.get('/test', response_class=HTMLResponse)
-async def tst(request: Request):
-    
-    return templates.TemplateResponse(
-        request=request,
-        name="test.html",
-        context={
-            "lol": "LOOOL"
-        }
-    )
+app = FastAPI(debug=True, title="Сайт")
 
 
 
-app.include_router(router=router)
+# html_test = "./templates/index.html"
+# html = None
+# with open(f"{html_test}", "r", encoding="utf-8") as f:
+#     html = f.read()
+
+@app.get("/")
+async def main() -> dict[str, int]:
+    return {"ok": 2}
+
+@app.post("/")
+async def main_post() -> dict[str, int]:
+    return {"ok status": 200}
+
+
+# @app.get("/home", response_class=HTMLResponse)
+# async def home():
+#     return html
+
+"""
+https://workspace.google.com/intl/ru/gmail/?search=ponchik URL
+https PROTOCOL 2  SSL -https / http 
+
+workspace.google.com SUB DOMEN/DNS
+google.com DOMEN/DNs
+DOMEN /intl/ru/gmail ENDPOINT
+QUERY PARAMETS ?search=ponchik
+
+http://127.0.0.1:8000
+"""
