9416c259a95674c94bde66d9bad47030210d668a
xam55 <islamhucaev09595@gmail.com>
2026-06-05 01:22:48 +0300

    коммит епт

--- app/repo/users.py
+++ app/repo/users.py
@@ -10,31 +10,39 @@ class User:
 
 
         async def create(
-                self,
-                user_id:int,
-                name:str,
-                email:str,):
-            user=User(id=user_id,name=name,email=email)
-            self.session.add(user)
-            await self.session.flush()
-            return user
-        
-
-        
-        async def get_user_by_id(user_id:int):
-            user=self.session.select(User).where(User.id==user_id)
-            return user.scalar_one_or_none()
-        
-
-        
-
+        self,
+        user_id: int,
+        name: str,
+        email: str,
+        hashed_password:str):
             
-
-        
-        
-
-
-   
+         user=User(id=user_id, name=name, email=email,hashed_password=hashed_password)
+         self.session.add(user)
+         self.session.flush()
+         return user
 
 
 
+    async def get_user_by_id(self,user_id:int):
+        user=self.session.select(User).where(User.id==user_id)
+        return user.scalar_one_or_none()
+
+
+    async def update_email(self, user_id:int, new_email:str):
+        user=await get_user_by_id(id)
+
+        if user == None:
+            return None
+
+        User.email=new_email
+        self.session.flush()
+        return user
+
+    async def update_password(self,user_id:id,new_password:str):
+        user=await get_user_by_id(user_id)
+        if user == None:
+            return None
+
+        User.hashed_password=new_password
+        self.session.flush()
+        return user
